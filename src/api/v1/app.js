8a4d4dc9451eabf5487d87c21ecaad8791fc1982
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2023-10-07 23:34:19 +0100

    updated webhook

--- src/api/v1/app.js
+++ src/api/v1/app.js
@@ -25,7 +25,7 @@ app.post(
   '/webhooks',
   asyncHandler((req, res) => {
     const hash = crypto
-      .createHmac('sha512', process.env.PAYSTACK_SECRET_KEY)
+      .createHmac('sha512', process.env.WEBHOOK_SECRET_KEY)
       .update(JSON.stringify(req.body))
       .digest('hex');
     console.log(req.headers['x-paystack-signature']);
